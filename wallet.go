5dd55f709c18d52337a68cc37800d4f59929f9a1
OlgaMalyshevaa <malysheva.oov@gmail.com>
2024-11-04 10:02:10 +0300

    first commit

---
+++ wallet.go
@@ -0,0 +1,37 @@
+package wallet
+
+import "fmt"
+import "errors"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance is: %p\n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if w.balance < amount {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
